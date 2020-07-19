8e45aa7b516c6db06f702056e54a044c22253719
Sergio Zacarias <sergiodcarp@gmail.com>
2020-07-19 13:53:34 -0300

    tp 1 ok

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -4,14 +4,35 @@ B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
-{
+{ 
+    var precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    var precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    var precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+
+    alert(`La suma es ${precioUno + precioDos + precioTres}`);
 	
 }
 function Promedio () 
-{
+{   
+    var precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    var precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    var precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+    var promedio = (precioUno + precioDos + precioTres) / 3;
+
+    alert (`El promedio es ${promedio}`);
 	
 }
 function PrecioFinal () 
-{
+{ 
+    var precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    var precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    var precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+    var precioTotal = precioUno + precioDos + precioTres;
+    var aumento = precioTotal *21/100;
+    var precioFinal = precioTotal + aumento;
+    
+
+    alert(`El precio final es  ${precioFinal}`);
+
 	
 }
