d43274faa3ff76261052419b312ccd7ba80ba7e9
Sergio Zacarias <sergiodcarp@gmail.com>
2020-07-29 22:25:22 -0300

    Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-09.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-09.js
@@ -1,6 +1,107 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+function mostrar() {
+	let estadiaBase = 15000;
+	let estacionDelAño;
+	let destino;
+	let aumento = 0;
+	let descuento = 0;
 
-}//FIN DE LA FUNCIÓN
+	estacionDelAño = document.getElementById("txtIdEstacion").value;
+	destino = document.getElementById("txtIdDestino").value;
+
+
+	switch (estacionDelAño) {
+
+		case "Invierno":
+
+			if (destino == "Bariloche") {
+
+				aumento = 20;
+				/*	aumento = estadiaBase + estadiaBase * aumento / 100;
+					alert(`Usted debe pagar $ ${aumento}.`); */
+			}
+			else if (destino == "Mar del plata") {
+				descuento = 20;
+				/*	descuento = estadiaBase - estadiaBase * descuento / 100;
+					alert(`Usted debe pagar $ ${descuento}.`); */
+
+
+			}
+			else {
+				descuento = 10
+				/*	descuento = estadiaBase - estadiaBase * descuento / 100;
+					alert(`Usted debe pagar $ ${descuento}.`);*/
+
+			}
+
+			break;
+
+
+		case "Verano":
+
+			if (destino == "Bariloche") {
+				descuento = 20;
+				/*	descuento = estadiaBase - estadiaBase * descuento / 100;
+					alert(`Usted debe pagar $ ${descuento}.`);*/
+
+			}
+			else if (destino == "Mar del plata") {
+				aumento = 20;
+				/*	aumento = estadiaBase + estadiaBase * aumento / 100;
+					alert(`Usted debe pagar $ ${aumento}.`);*/
+			}
+			else {
+				aumento = 10;
+				/*	aumento = estadiaBase + estadiaBase * aumento / 100;
+					alert(`Usted debe pagar $ ${aumento}.`);*/
+			}
+
+			break;
+
+
+		case "Otoño":
+		case "Primavera":
+
+			if (destino == "Cordoba") {
+				alert(`Usted debe pagar $${estadiaBase}`);
+				/*	alert(`Usted debe pagar $ ${estadiaBase}.`);*/
+			}
+			else {
+				aumento = 10;
+				/*	aumento = estadiaBase + estadiaBase * aumento / 100;
+					alert(`Usted debe pagar $ ${aumento}.`) */
+			}
+			break;
+
+		default:
+			alert("No indico una estacion del año o destino correcto.");
+			break;
+	}
+	if (aumento != 0) {
+		aumento = estadiaBase + estadiaBase * aumento / 100;
+		alert(`Usted debe pagar $${aumento}.`);
+
+	}
+	else if (descuento != 0) {
+		descuento = estadiaBase - estadiaBase * descuento / 100;
+		alert(`Usted debe pagar $${descuento}.`);
+	}
+
+}
+
+/* Enunciado:
+una agencia de viajes debe sacar las tarifas de los viajes , se cobra $15.000 por cada estadia como base,
+
+ se pide el ingreso de una estacion del año y localidad para vacacionar para poder calcular el precio final
+
+en Invierno: bariloche tiene un aumento del 20% cataratas
+ y Cordoba tiene un descuento del 10%
+  Mar del plata tiene un descuento del 20%
+
+en Verano: bariloche tiene un descuento del 20%
+catarata y Cordoba tiene un aumento del 10%
+  Mar del plata tiene un aumento del 20%
+
+en Otoño y Primavera: bariloche tiene un aumento del 10%
+ cataratas tiene un aumento del 10%
+  Mar del plata tiene un aumento del 10%
+   y Cordoba tiene el precio sin descuento*/
--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,66 @@
-function mostrar()
-{
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+function mostrar() {
+	let estacionDelAño;
+	let destino;
 
-}//FIN DE LA FUNCIÓN
+	estacionDelAño = document.getElementById("txtIdEstacion").value;
+	destino = document.getElementById("txtIdDestino").value;
+
+	switch (estacionDelAño) {
+
+		case "Invierno":
+			if (destino == "Bariloche") {
+				alert("Se viaja.");
+			}
+			else {
+				alert("No se viaja.");
+			}
+			break;
+
+
+		case "Verano":
+			if (destino == "Mar del plata" || destino == "Cataratas") {
+				alert("Se viaja.");
+			}
+			else {
+				alert("No se viaja.");
+			}
+			break;
+
+
+		case "Otoño":
+			alert("Se viaja.")
+			break;
+
+
+		case "Primavera":
+			if (destino == "Bariloche")
+				alert("No se viaja.");
+
+			else {
+				alert("Se viaja.")
+			}
+			break;
+
+		default:
+			alert("No indico una estacion del año o destino correcto.");
+			break;
+
+
+
+
+	}
+
+
+}
+
+/*Enunciado:
+una agencia de viajes nos piden informar si hacemos viajes a lugares según la estación del año estemos,
+ informar si "Se viaja" o "No se viaja" a ese lugar
+
+en Invierno: Solo Bariloche informa "se viaja" los demas destinos "No se viaja"
+
+en Verano: Se viaja a Mar del plata y Cataratas solamente
+
+en Otoño: Se viaja a todos los destinos.
+
+primavera: solo no se viaja a Bariloche */
